d2cf7152275f7cd09334bf41e753c1cda0db31ac
John Egan <eganj@dnb.com>
2019-06-23 15:01:14 -0500

    Exclude nulls in JSON

--- src/main/java/com/shinycatcher/api/dto/EntryDto.java
+++ src/main/java/com/shinycatcher/api/dto/EntryDto.java
@@ -1,5 +1,9 @@
 package com.shinycatcher.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public class EntryDto {
 	
 	public Long entryId;
