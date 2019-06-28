21d7aa9e77520cdcd24d27f3c3f46e74a2c7d04d
John Egan <eganj@dnb.com>
2019-06-27 20:58:20 -0500

    Added service annotation

--- src/main/java/com/shinycatcher/api/service/SuggestionsService.java
+++ src/main/java/com/shinycatcher/api/service/SuggestionsService.java
@@ -3,10 +3,12 @@ package com.shinycatcher.api.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.shinycatcher.api.dao.UserDao;
 import com.shinycatcher.api.dto.EntryDetailsDto;
 
+@Service
 public class SuggestionsService {
 	
 	@Autowired
