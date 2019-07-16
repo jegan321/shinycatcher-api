59eb501d5bf5922edd38c5abd8e99123adc1f648
Larnth <larnth@shinydex.com>
2019-07-15 21:27:12 -0500

    Remove unused import.

--- src/main/java/com/shinycatcher/api/dao/mapper/PokedexDao.java
+++ src/main/java/com/shinycatcher/api/dao/mapper/PokedexDao.java
@@ -1,7 +1,6 @@
 package com.shinycatcher.api.dao.mapper;
 
 import com.shinycatcher.api.entity.Pokedex;
-import com.shinycatcher.api.entity.Pokemon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
