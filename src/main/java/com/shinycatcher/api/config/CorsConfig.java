e7a67f8a199ffc92af32e3021f7514d884ed48dd
John Egan <eganj@dnb.com>
2019-07-17 18:20:51 -0500

    Fixed CORS issue

--- src/main/java/com/shinycatcher/api/config/CorsConfig.java
+++ src/main/java/com/shinycatcher/api/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.shinycatcher.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class CorsConfig {
-	
+
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
